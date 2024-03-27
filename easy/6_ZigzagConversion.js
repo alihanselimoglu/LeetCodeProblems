22bc1df03d7919882b373f461125bdd3de2aebc3
Alihan <82150661+alienslimeson@users.noreply.github.com>
2024-03-28 00:23:20 +0300

    6_ZigzagConversion

---
+++ easy/6_ZigzagConversion.js
@@ -0,0 +1,61 @@
+/*
+The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+
+P   A   H   N
+A P L S I I G
+Y   I   R
+And then read line by line: "PAHNAPLSIIGYIR"
+
+Write the code that will take a string and make this conversion given a number of rows:
+
+string convert(string s, int numRows);
+ 
+
+Example 1:
+
+Input: s = "PAYPALISHIRING", numRows = 3
+Output: "PAHNAPLSIIGYIR"
+Example 2:
+
+Input: s = "PAYPALISHIRING", numRows = 4
+Output: "PINALSIGYAHRPI"
+Explanation:
+P     I    N
+A   L S  I G
+Y A   H R
+P     I
+Example 3:
+
+Input: s = "A", numRows = 1
+Output: "A"
+ 
+
+Constraints:
+
+1 <= s.length <= 1000
+s consists of English letters (lower-case and upper-case), ',' and '.'.
+1 <= numRows <= 1000
+*/
+
+/**
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+var convert = function(s, numRows) {
+    if(s.length <= numRows || numRows == 1 ) return s;
+
+    let direction = false;
+    let count = 0;
+
+    let arr = new Array(numRows).fill('')
+    for(let i = 0; i < s.length; i++) {
+        let curr = s[i]
+
+        arr[count] += curr
+        if(count === 0 || count >= numRows - 1) direction = !direction
+        direction ? count++ : count--
+    }
+    return arr.join('')
+
+};
