05d5bd3b01bc636cabc38a634fe94ecbdb219b89
Alihan <82150661+alienslimeson@users.noreply.github.com>
2024-05-03 08:23:38 +0300

    15_3Sum

---
+++ easy/15_3Sum.js
@@ -0,0 +1,68 @@
+/*
+15. 3Sum
+Solved
+Medium
+Topics
+Companies
+Hint
+Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+Notice that the solution set must not contain duplicate triplets.
+
+ 
+
+Example 1:
+
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
+Explanation: 
+nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+The distinct triplets are [-1,0,1] and [-1,-1,2].
+Notice that the order of the output and the order of the triplets does not matter.
+Example 2:
+
+Input: nums = [0,1,1]
+Output: []
+Explanation: The only possible triplet does not sum up to 0.
+Example 3:
+
+Input: nums = [0,0,0]
+Output: [[0,0,0]]
+Explanation: The only possible triplet sums up to 0.
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+    nums.sort((a, b) => a - b)
+    let set = new Set()
+    
+    nums.forEach((num, index) => {
+        let left = index + 1
+        let right = nums.length - 1
+
+        while (left < right) {
+            const sum = num + nums[left] + nums[right]
+            if (sum === 0) {
+                set.add([num, nums[left], nums[right]].sort().toString())
+                left++
+                right--
+            } else if (sum < 0) {
+                left++
+            } else {
+                right--
+            }
+        }
+    }) 
+
+
+    return Array.from(set).map(triplet => triplet.split(',').map(Number));
+}
+
+
+
+
