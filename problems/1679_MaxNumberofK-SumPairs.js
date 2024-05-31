fa6e1c44d21753ab790a52d6d8df4f3c27100425
Alihan <82150661+alienslimeson@users.noreply.github.com>
2024-05-31 16:47:02 +0300

    1679_MaxNumberofK-SumPairs

---
+++ problems/1679_MaxNumberofK-SumPairs.js
@@ -0,0 +1,63 @@
+/*
+1679. Max Number of K-Sum Pairs
+Solved
+Medium
+Topics
+Companies
+Hint
+You are given an integer array nums and an integer k.
+
+In one operation, you can pick two numbers from the array whose sum equals k and remove them from the array.
+
+Return the maximum number of operations you can perform on the array.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4], k = 5
+Output: 2
+Explanation: Starting with nums = [1,2,3,4]:
+- Remove numbers 1 and 4, then nums = [2,3]
+- Remove numbers 2 and 3, then nums = []
+There are no more pairs that sum up to 5, hence a total of 2 operations.
+Example 2:
+
+Input: nums = [3,1,3,4,3], k = 6
+Output: 1
+Explanation: Starting with nums = [3,1,3,4,3]:
+- Remove the first two 3's, then nums = [1,4,3]
+There are no more pairs that sum up to 6, hence a total of 1 operation.
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+1 <= nums[i] <= 109
+1 <= k <= 109
+*/
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var maxOperations = function(nums, k) {
+    nums.sort((a, b) => a - b);
+    let l = 0, r = nums.length - 1;
+    let count = 0;
+
+    while(l < r) {
+        let sum = nums[l] + nums[r];
+
+        if(sum === k) {
+            count++;
+            l++;
+            r--;
+        } else if(sum < k) {
+            l++;
+        } else r--;
+    }
+
+    return count
+};
